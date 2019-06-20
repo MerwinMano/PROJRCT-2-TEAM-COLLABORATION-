d3c55c5620ca3d62e4d1b020ffcf0b13a4dbd07b
Merwin <Merwin@192.168.1.43>
2019-06-20 16:32:15 +0530

    DB config file created

---
+++ colloboration_backend/src/main/java/mer/mano/configuration/DBConfiguration.java
@@ -0,0 +1,65 @@
+package mer.mano.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import mer.mano.model.BlogComment;
+import mer.mano.model.BlogPost;
+import mer.mano.model.BlogPostLikes;
+import mer.mano.model.Friend;
+import mer.mano.model.Job;
+import mer.mano.model.Notification;
+import mer.mano.model.ProfilePicture;
+import mer.mano.model.User;
+
+
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfiguration {
+	
+	public DBConfiguration(){
+		System.out.println("DBCOnfiguration class instantiated");
+	}
+	@Bean
+	public SessionFactory sessionFactory() {
+		LocalSessionFactoryBuilder lsf=
+				new LocalSessionFactoryBuilder(getDataSource());
+		Properties hibernateProperties=new Properties();
+		hibernateProperties.setProperty(
+				"hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		lsf.addProperties(hibernateProperties);
+		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,BlogPostLikes.class,Notification.class,BlogComment.class,ProfilePicture.class,Friend.class};
+	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
+	}
+	@Bean
+	public DataSource getDataSource() {
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+	    dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:admin");
+	    dataSource.setUsername("SYSTEM");
+	    dataSource.setPassword("Admin12345");
+	    return dataSource;
+	    
+	}
+	@Bean
+	public HibernateTransactionManager hibTransManagement(){
+		return new HibernateTransactionManager(sessionFactory());
+	}
+
+}
+
+	
+	
+
