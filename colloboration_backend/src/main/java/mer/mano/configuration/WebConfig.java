4b2fb40caae2c8738e8c35ac76bcac3eefffc0af
Merwin <Merwin@192.168.1.43>
2019-06-20 16:33:05 +0530

    web configuration files commited

---
+++ colloboration_backend/src/main/java/mer/mano/configuration/WebConfig.java
@@ -0,0 +1,27 @@
+package mer.mano.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="mer.mano")
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	public WebConfig(){
+		System.out.println("WEBCONFIG class is instantiated");
+
+}
+	@Bean(name="multipartResolver")
+	public CommonsMultipartResolver getCommonsMultipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(20971520); // 20MB
+		multipartResolver.setMaxInMemorySize(1048576);	// 1MB
+		return multipartResolver;
+	}	
+}
